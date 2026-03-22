105aca1cbdcd5e6288c837c9f90bff53de879d6b
Rudra Das <154529423+rudradas05@users.noreply.github.com>
2026-03-22 19:45:51 +0530

    Time: 2 ms (77.09%), Space: 165.9 MB (35.6%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int i =0;
-        for(i =0; i< nums1.size() ; i++){
-            if(nums1[i]%2==1)break;
-        }
-        if(i == nums1.size())return true;
+        
 
         // try to get all odds
         int minele = INT_MAX;
@@ -13,6 +9,7 @@ public:
             if(nums1[i]%2==1)minele = min(minele, nums1[i]);
 
         }
+        if(minele== INT_MAX)return true;
         for(int i=0; i< nums1.size(); i++){
             if(nums1[i]%2==0 && nums1[i]<minele) return false;
         }
