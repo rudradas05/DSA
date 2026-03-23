82abab5cd89c8628fbbba99c4d1e7a582df9f78c
Rudra Das <154529423+rudradas05@users.noreply.github.com>
2026-03-23 22:18:06 +0530

    Time: 480 ms (22.85%), Space: 100.9 MB (5%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     int totalSum;
-    vector<vector<int>> dp;
+    unordered_map<string, int> mp;
+
     int solve(int ind, int sum, vector<int>& nums, int target) {
+        string str ="";
+        if(ind> nums.size()) return 0;
+        str = to_string(sum)+"_"+to_string(ind);
+        if (mp.find(str)!=mp.end())return mp[str];
         if (ind == nums.size()) {
-            return (sum == target) ? 1 : 0;
+            if (sum == target) {
+                return mp[str]=1;
+            }else{
+                return mp[str]=0;
+            }
         }
-        if (dp[ind][sum + totalSum] != -1)
-            return dp[ind][sum + totalSum];
+        
         int add = solve(ind + 1, sum + nums[ind], nums, target);
         int subs = solve(ind + 1, sum - nums[ind], nums, target);
-        return dp[ind][sum+totalSum] = add + subs;
+        return mp[str] = add + subs;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-        dp.resize(n, vector<int>(2*totalSum+1, -1));
+        
+
         return solve(0, 0, nums, target);
     }
 };
